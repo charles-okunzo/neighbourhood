53a64655fa3a31ffbc2fb4a3a0179374daa0b3fa
Guyo <you@example.com>
2022-06-18 13:06:55 +0300

    feature --create register view class

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+
+from users.forms import UserRegisterForm
 
 # Create your views here.
+
+class Register(CreateView):
+    model = User
+    form_class = UserRegisterForm
